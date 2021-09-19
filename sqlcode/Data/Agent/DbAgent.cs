f07c8a63c6712f36e1532fd9bbd26ab71e142923
F Jiang <fjiang2@github.com>
2021-09-19 12:49:46 -0500

    Added method Quey(SQL, args) to DbAgent

--- sqlcode/Data/Agent/DbAgent.cs
+++ sqlcode/Data/Agent/DbAgent.cs
@@ -32,6 +32,17 @@ namespace Sys.Data
 
 		public DataQuery Query() => new DataQuery(this);
 
+		public DbAccess Query(string sql, object args = null)
+		{
+			var unit = new SqlUnit(sql, args);
+			
+			var proxy = Proxy(unit);
+			if (proxy is DbAccess access)
+				return access;
+			else
+				return new DbAccessDelegate(this, unit);
+		}
+
 		public override string ToString()
 		{
 			return Option.ToString();
