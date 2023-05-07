0507ceaf6cbe222e750d61db3ebd19afd51d2275
FJ <fjiang@github.com>
2023-05-07 11:20:22 -0500

    Created project sqlcode.dynamodb

---
+++ sqlcode.dynamodb/Entity/EntityValue.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using sqlcode.dynamodb.extensions;
+
+namespace sqlcode.dynamodb.entity
+{
+    public class EntityValue
+    {
+        public Type Type { get; set; }
+        public object Value { get; set; }
+
+        public EntityValue(object value)
+            : this(value.GetType(), value)
+        {
+        }
+
+        public EntityValue(Type type, object value)
+        {
+            Type = type;
+            Value = value;
+        }
+
+        public object this[string name]
+        {
+            get
+            {
+                if (Value is IDictionary<string, object> dict)
+                {
+                    return dict[name];
+                }
+
+                throw new InvalidOperationException($"IDictionary<string,object> expected: {Type.FullName}");
+            }
+            set
+            {
+                if (Value is IDictionary<string, object> dict)
+                {
+                    dict[name] = value;
+                }
+
+                throw new InvalidOperationException($"IDictionary<string,object> expected: {Type.FullName}");
+            }
+        }
+
+        public override string ToString()
+        {
+            if (Value is System.Collections.IEnumerable)
+                return Json.Serialize(Value);
+            else
+                return $"{Value}";
+        }
+    }
+}
---
+++ sqlcode.dynamodb/clients/IDynamoDBClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace sqlcode.dynamodb.clients
+{
+    public interface IDynamoDBClient
+    {
+        Task CreateTable(string tableName, DataColumn partitionKey);
+        Task CreateTable(string tableName, DataColumn partitionKey, DataColumn sortKey);
+        Task CreateTable(string tableName, string partitionKey, DynamoDataType partitionType);
+        Task CreateTable(string tableName, string partitionKey, DynamoDataType partitionType, string sortKey, DynamoDataType sortType);
+
+        Task<bool> Exists(string tableName, TimeSpan timeout);
+    }
+}
