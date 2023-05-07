11be28a2d1f8a34dbd44957267b9b9dd7a9d51ef
FJ <fjiang@github.com>
2023-05-07 11:53:07 -0500

    Updated nuget packages

---
+++ UnitTestProject/SqlCe/UnitTest_SqlCe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+#if NET48
+using Sys.Data.SqlCe;
+
+namespace UnitTestProject.sqlce
+{
+    [TestClass]
+    public class UnitTest_SqlCe
+    {
+    }
+}
+#endif
