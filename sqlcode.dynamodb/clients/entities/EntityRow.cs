05c4988eeb1500dce390d8fa7496697944e72136
FJ <fjiang@github.com>
2023-07-31 19:15:24 -0500

    Added entity row/table back

---
+++ sqlcode.dynamodb/clients/entities/EntityRow.cs
@@ -0,0 +1,17 @@
+﻿using sqlcode.dynamodb.extensions;
+
+namespace sqlcode.dynamodb.clients.entities
+{
+    public class EntityRow : Dictionary<string, EntityValue>
+    {
+        public EntityRow()
+        {
+        }
+
+        public override string ToString()
+        {
+            var dict = this.ToDictionary(x => x.Key, x => x.Value.Value);
+            return Json.Serialize(dict);
+        }
+    }
+}
---
+++ sqlcode.dynamodb/clients/entities/EntityTable.cs
@@ -0,0 +1,41 @@
+﻿using System.Data;
+
+namespace sqlcode.dynamodb.clients.entities
+{
+    public class EntityTable : List<EntityRow>
+    {
+        public EntityTable()
+        {
+        }
+
+        public DataTable ToDataTable()
+        {
+            var dt = new DataTable();
+            foreach (var row in this)
+            {
+                foreach (var col in row)
+                {
+                    string columnName = col.Key;
+                    if (!dt.Columns.Contains(columnName))
+                    {
+                        dt.Columns.Add(new DataColumn(columnName, col.Value.Type));
+                    }
+                }
+            }
+
+            foreach (var row in this)
+            {
+                var newRow = dt.NewRow();
+                foreach (var col in row)
+                {
+                    newRow[col.Key] = col.Value.Value;
+                }
+
+                dt.Rows.Add(newRow);
+            }
+
+            dt.AcceptChanges();
+            return dt;
+        }
+    }
+}
