16936f5e6ad5540b8f740e33af809b8a6740b60b
FJ <fjiang@github.com>
2024-11-30 14:15:38 -0600

    keep changes

--- sqlcode/Entity/IDbContext.cs
+++ sqlcode/Entity/IDbContext.cs
@@ -13,6 +13,9 @@ namespace Sys.Data.Entity
         event EventHandler<RowEventArgs> RowChanged;
         event EventHandler<RowEventArgs> RowChanging;
 
+        /// <summary>
+        /// Clear generated code and row events
+        /// </summary>
         void Clear();
 
         IEnumerable<TSubEntity> Expand<TEntity, TSubEntity>(IEnumerable<TEntity> entities) 
@@ -30,6 +33,10 @@ namespace Sys.Data.Entity
         Type[] ExpandOnSubmit<TEntity>(IEnumerable<TEntity> entities) where TEntity : class;
         Type[] ExpandOnSubmit<TEntity>(TEntity entity) where TEntity : class;
 
+        /// <summary>
+        /// It can be used for Bulk copy/insert
+        /// </summary>
+        /// <returns>key is table-name, value is an array of INSERT statements</returns>
         IDictionary<Type, string[]> GetBulkInsert();
         string GetNonQueryScript();
         string GetQueryScript();
