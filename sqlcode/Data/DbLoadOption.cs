6d24708d07ced6e23d9932fdc6d58c6e8b636ad7
FJ <fjiang@github.com>
2024-12-01 12:20:42 -0600

    Updated comments

--- sqlcode/Data/DbLoadOption.cs
+++ sqlcode/Data/DbLoadOption.cs
@@ -1,5 +1,8 @@
 ﻿namespace Sys.Data
 {
+    /// <summary>
+    /// Define how to load rows from database server
+    /// </summary>
     public struct DbLoadOption
     {
         public static readonly DbLoadOption DefaultOption = new DbLoadOption
@@ -9,14 +12,19 @@
             Mode = DbLoadMode.DbFill
         };
 
+        /// <summary>
+        /// Use database adapter or database reader
+        /// </summary>
         public DbLoadMode Mode { get; set; }
 
         /// <summary>
-        /// start-record
+        /// Start record
+        /// The zero-based record number to start with
         /// </summary>
         public int StartRecord { get; set; }
 
         /// <summary>
+        /// Maximum number of records to retrieve.
         /// Number of records. -1: all records from start-record
         /// </summary>
         public int MaxRecords { get; set; }
--- sqlcode/Entity/IDbQuery.cs
+++ sqlcode/Entity/IDbQuery.cs
@@ -23,6 +23,13 @@ namespace Sys.Data.Entity
         IEnumerable<TSubEntity> Expand<TEntity, TSubEntity>(IEnumerable<TEntity> entities)
             where TEntity : class
             where TSubEntity : class;
+
+        /// <summary>
+        ///  Expand association tables with foreign keys
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <param name="entities"></param>
+        /// <returns></returns>
         IQueryResultReader Expand<TEntity>(IEnumerable<TEntity> entities) where TEntity : class;
 
         int Insert<TEntity>(IEnumerable<TEntity> entities) where TEntity : class;
@@ -30,15 +37,68 @@ namespace Sys.Data.Entity
         int InsertOrUpdateRow<TEntity>(TEntity entity) where TEntity : class;
         int InsertRow<TEntity>(TEntity entity) where TEntity : class;
 
+        /// <summary>
+        /// Update partial columns of entity, values of primary key required
+        /// example of partial entity
+        ///   1.object: new { Id=7, Name="XXXX"} 
+        ///   2.Dictionary: new Dictionary&lt;string, object&gt;{["Id"]=7, ["Name"]="XXXX"}</string>
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <param name="entities"></param>
+        /// <param name="throwException"></param>
+        /// <returns></returns>
         int PartialUpdate<TEntity>(IEnumerable<object> entities, bool throwException = false) where TEntity : class;
+
+        /// <summary>
+        /// Update entity partially
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <param name="entity"></param>
+        /// <param name="modifiedProperties">The properties are modified</param>
+        /// <param name="where"></param>
+        /// <returns></returns>
         int PartialUpdate<TEntity>(TEntity entity, Expression<Func<TEntity, object>> modifiedProperties, Expression<Func<TEntity, bool>> where) where TEntity : class;
+
+        /// <summary>
+        /// Update a single row with entity partially
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <param name="entity"></param>
+        /// <param name="throwException"></param>
+        /// <returns></returns>
         int PartialUpdateRow<TEntity>(object entity, bool throwException = false) where TEntity : class;
 
         IQueryResultReader Select(Action<DbContext> action);
+
+        /// <summary>
+        /// SELECT * FROM entity-table WHERE key-selector IN (SELECT result-selector FROM result-table WHERE ...)
+        /// e.g.
+        ///   Query.Select<Categories, int, Products>(row => row.CategoryName == "Beverages", row => row.CategoryID, row => row.CategoryID);
+        ///   SELECT * FROM [Products] WHERE CategoryID IN (SELECT CategoryID FROM Categories WHERE CategoryName == 'Beverages')
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="where"></param>
+        /// <param name="keySelector"></param>
+        /// <param name="resultSelector"></param>
+        /// <returns></returns>
         IEnumerable<TResult> Select<TEntity, TResult>(Expression<Func<TEntity, bool>> where, Expression<Func<TEntity, object>> keySelector, Expression<Func<TResult, object>> resultSelector)
             where TEntity : class
             where TResult : class;
+
+        /// <summary>
+        /// Get all rows of a table
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <returns></returns>
         IEnumerable<TEntity> Select<TEntity>() where TEntity : class;
+
+        /// <summary>
+        /// Use SqlBuilder to select rows
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <param name="where"></param>
+        /// <returns></returns>
         IEnumerable<TEntity> Select<TEntity>(Text.Expression where) where TEntity : class;
         IEnumerable<TEntity> Select<TEntity>(Expression<Func<TEntity, bool>> where) where TEntity : class;
         IEnumerable<TEntity> Select<TEntity>(Expression<Func<TEntity, bool>> where, DbLoadOption option) where TEntity : class;
@@ -51,7 +111,21 @@ namespace Sys.Data.Entity
         
         int Update<TEntity>(IEnumerable<TEntity> entities) where TEntity : class;
         int UpdateRow<TEntity>(TEntity entity) where TEntity : class;
+
+        /// <summary>
+        /// Support SqlCe and SQL server
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <param name="entities"></param>
+        /// <returns></returns>
         int Upsert<TEntity>(IEnumerable<TEntity> entities) where TEntity : class;
+
+        /// <summary>
+        /// Support SqlCe and SQL server, use primary key to check row existence
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <param name="entity"></param>
+        /// <returns></returns>
         int UpsertRow<TEntity>(TEntity entity) where TEntity : class;
     }
 }
